63cf715eb2deededc02efc247f59a1e4bcc2e40b
ramachandran <ramachandran@cloudmint.com>
2016-01-26 22:43:49 +0530

    changes in cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'todoapp'
 set :repo_url, 'ssh://ubuntu@54.254.155.224:/home/ubuntu/repos/todoapp.git'
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -15,12 +15,13 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}/public"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_bind,	      "tcp://127.0.0.1:9292"
+#set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+#set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+#set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "/home/ubuntu/apps/todoapp/logs/puma-production.stderr.log"
+set :puma_error_log,  "/home/ubuntu/apps/todoapp/logs/puma-production.stdout.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user) } #, keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -79,17 +80,17 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
+#namespace :puma do
+  #desc 'Create Directories for Puma Pids and Socket'
+#  task :make_dirs do
+#    on roles(:app) do
+#      execute "puma -C config/puma.rb"
+      #execute "mkdir #{shared_path}/tmp/pids -p"
+#    end
+#  end
 
-  before :start, :make_dirs
-end
+#  before :start, :make_dirs
+#end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
