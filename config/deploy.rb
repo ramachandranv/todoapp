07f69ecedb4b5650a70d2982b71d0e699c68a75f
Ramachandran <ramachandran@cloudmint.com>
2016-01-27 12:10:28 +0530

    cap changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,10 +49,10 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -82,8 +82,8 @@ set :keep_releases, 1
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 #namespace :puma do
   #desc 'Create Directories for Puma Pids and Socket'
@@ -130,7 +130,7 @@ namespace :deploy do
 
   #before :starting,     :check_revision
   after  :finishing,    :compile_assets
-  after  :finishing,    :symlink_config_files
+  # after  :finishing,    :symlink_config_files
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
